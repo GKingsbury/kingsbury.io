6cc6e49dc52dd0017711ce27bef6b4f41cb2a53c
Grant Kingsbury <6866640+GKingsbury@users.noreply.github.com>
2022-01-14 20:57:07 -0500

    Adding transition on load

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,12 @@ import TechButton from './TechButton.vue'
 
 defineProps<{ msg: string }>()
 
+let show = ref(false)
+
+setTimeout(() => {
+  show.value = true
+}, 200)
+
 interface TechButton {
   img: string,
   description: string
@@ -27,21 +33,36 @@ const techList: TechButton[] = [
 
 <template>
 <div class="min-h-screen bg-gradient-to-l from-blue-200 to-purple-300 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12">
-  <div class="relative pt-10 pb-8 bg-gray-100 shadow-xl ring-2 ring-offset-2 ring-gray-900/5 md:w-1/2 w-5/6 mx-auto rounded-xl px-10">
-    <div class="mx-auto" >
-      <div class="sm:text-5xl text-4xl text-center mb-5"><span class="font-bold text-indigo-900">Kingsbury</span><span class="text-gray-300">.us</span> </div>
-      <hr/>
-    </div>
-    <div class="mx-auto flex flex-wrap my-10 w-5/6">
-      <div class="lg:basis-1/2 p-2 text-gray-400">
-        Hey. Welcome to my site. My name is <span class="font-bold text-indigo-900">Grant Kingsbury</span>. I am a full stack developer with over 7 years of experience.
-        I love building new things. I'm always trying to find new technologies that makes my job easier, faster, and enjoyable. 
+  <transition name="fade" >
+    <div v-if="show" class="relative pt-10 pb-8 bg-gray-100 shadow-xl ring-2 ring-offset-2 ring-gray-900/5 md:w-1/2 w-5/6 mx-auto rounded-xl px-10">
+      <div class="mx-auto" >
+        <div class="sm:text-5xl text-4xl text-center mb-5"><span class="font-bold text-indigo-900">Kingsbury</span><span class="text-gray-300">.us</span> </div>
+        <hr/>
       </div>
-      <div class="lg:basis-1/2 p-2 flex flex-wrap">
-        <TechButton v-for="tech in techList" :key="tech.description" :img="tech.img"/>
+      <div class="mx-auto flex flex-wrap my-10 w-5/6">
+        <div class="lg:basis-1/2 p-2 text-gray-400">
+          Hey. Welcome to my site. My name is <span class="font-bold text-indigo-900">Grant Kingsbury</span>. I am a full stack developer with over 7 years of experience.
+          I love building new things. I'm always trying to find new technologies that makes my job easier, faster, and enjoyable. 
+        </div>
+        <div class="lg:basis-1/2 p-2 flex flex-wrap">
+          <TechButton v-for="tech in techList" :key="tech.description" :img="tech.img"/>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </div>
 
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateY(30px);
+  opacity: 0;
+}
+</style>
